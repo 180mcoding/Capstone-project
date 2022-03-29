a735cbab496beee6d755fc6588598fca82045e8f
180mcoding <84143799+180mcoding@users.noreply.github.com>
2022-03-29 12:21:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function Secret()
+{
+     document.getElementById("img1").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbY6WPpsIS_Uyp7kFhBlRqoafp-wqgNW_nCA&usqp=CAU"
+}
+
+function Back()
+{
+     document.getElementById("img1").src="https://tse2.mm.bing.net/th?id=OIP.LmIykrZi1tDGj-YIk5aNVQHaFj&pid=Api&P=0&w=225&h=168"
+}
